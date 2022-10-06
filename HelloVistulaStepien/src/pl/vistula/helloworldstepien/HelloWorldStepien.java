49444b4161c0f489b013e1d2ba9d82bd440bc3bb
TomaszStepienVistula <114996217+TomaszStepienVistula@users.noreply.github.com>
2022-10-06 16:03:55 +0200

    Testowy commit

---
+++ HelloVistulaStepien/src/pl/vistula/helloworldstepien/HelloWorldStepien.java
@@ -0,0 +1,11 @@
+package pl.vistula.helloworldstepien;
+
+public class HelloWorldStepien {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Vistula Stępień");
+
+    }
+
+}
